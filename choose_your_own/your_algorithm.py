f26b23a8c500257f479e3d4e601ecb820c5ec9c5
jaysunice3401 <jasonaizkalns@msn.com>
2014-11-19 14:57:49 -0500

    adaboost in your_algorithm.py

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -28,15 +28,19 @@ plt.show()
 #################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
-
-
-
-
-
-
-
+from sklearn.ensemble import AdaBoostClassifier
+from time import time
+clf = AdaBoostClassifier(n_estimators=100)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time() - t0, 3), "s"
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time: ", round(time() - t1, 3), "s"
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
 
 try:
     prettyPicture(clf, features_test, labels_test)
